cdac70ebb5ef32ae62720f117212a5bf06aeac96
C-Y-P-H-E-R <quasarkid123@gmail.com>
2021-09-28 13:41:35 +0530

    complete-dnd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import { useDropzone } from 'react-dropzone';
+import { useMemo, useState } from 'react'
 import './App.css';
 
+const baseStyle = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '20px',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: '#eeeeee',
+  borderStyle: 'dashed',
+  backgroundColor: '#fafafa',
+  color: '#bdbdbd',
+  outline: 'none',
+  transition: 'border .24s ease-in-out'
+};
+
+const activeStyle = {
+  borderColor: '#2196f3'
+};
+
+const acceptStyle = {
+  borderColor: '#00e676'
+};
+
+const rejectStyle = {
+  borderColor: '#ff1744'
+};
+
 function App() {
+  const {
+    acceptedFiles, 
+    rejectedFiles, 
+    getRootProps, 
+    getInputProps, 
+    isDragActive, 
+    isDragAccept, 
+    isDragReject
+  } = useDropzone({
+    accept: ".pdf"
+  });
+
+  // const [readfile,setReadFile] = useState();
+
+  function displayAcceptedFilesContent(file) {
+      const fileData = file[0]
+      // console.log(Object.getPrototypeOf(fileData))
+      const blob = new Blob([JSON.stringify(fileData)], {type : 'application/json'});
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        e.preventDefault()
+        console.log(e.target);
+      };
+      console.log(blob)
+      reader.readAsText(blob);
+  }
+
+  const style = useMemo(() => ({
+    ...baseStyle,
+    ...(isDragActive ? activeStyle : {}),
+    ...(isDragAccept ? acceptStyle: {}),
+    ...(isDragReject ? rejectStyle: {})
+  }),
+  [
+    isDragAccept,
+    isDragActive,
+    isDragReject,
+  ])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Building a Drag-n-drop stl file-read application!</h1>
+      <div className="dnd" {...getRootProps({style})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <div className="file_contents">
+        {
+          (acceptedFiles == null) ? {} : displayAcceptedFilesContent(acceptedFiles) 
+        }
+      </div>
     </div>
   );
 }
